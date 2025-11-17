533f47df3d4c6e9a15d4a0bb3a2163689c512f38
Mahadi Xion <mahadi.xion@urz.uni-heidelberg.de>
2025-11-17 16:14:05 +0100

    Add DebugMode
    - Replace load_basic_debug_mode with DebugMode
    - Remove MainAppCLIDoc.config_file patching
    - Update Rya to 0.2.32

--- src/myapp/cli/__main__.py
+++ src/myapp/cli/__main__.py
@@ -1,13 +1,7 @@
 from rya.cli import initiate_cli_startup
-from rya.cli.doc import MainAppCLIDoc
 
 from ..names import AppIdentity
 from .app import app
 
-
-MainAppCLIDoc.config_file = (
-    f"Configuration file with the highest priority. E.g., [green]{AppIdentity.app_name} --C "
-    f"./project_config.{AppIdentity.config_file_extension} <command>[/green]."
-)
 initiate_cli_startup(app)
 app(prog_name=AppIdentity.app_name)
